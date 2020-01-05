55905c7b202013497ff8a68e6d1f0854ffc9dabc
Vitalii Borodulin <vitaliiborodulin@yandex.ru>
2020-01-05 09:55:30 +0300

    add template for specialists section

---
+++ page-team.php
@@ -0,0 +1,23 @@
+<?php
+
+/* 
+Template Name: Наша команда 
+*/
+
+?>
+
+<?php get_header(); ?>
+
+<div class="specialists">
+    <div class="container">
+        <div class="title">Наша команда</div>
+        <div class="row">
+            <div class="col-lg-10 offset-lg-1">
+                <img class="specialists__img" src="<?php bloginfo('template_url'); ?>/assets/img/team.jpg"
+                    alt="наша команда">
+            </div>
+        </div>
+    </div>
+</div>
+
+<?php get_footer();?>
