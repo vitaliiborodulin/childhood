1b8a8c6469b6320b1c8b2f289420ef8891c7f9a9
Vitalii Borodulin <vitaliiborodulin@yandex.ru>
2020-01-05 09:52:05 +0300

    remove unneccessary comments

--- functions.php
+++ functions.php
@@ -18,15 +18,4 @@
     add_theme_support( 'custom-logo' );
     add_theme_support( 'post-thumbnails' );
 
-    // function my_acf_google_map_api( $api ){
-    
-    //     $api['key'] = 'AIzaSyBUsOn-0meH9I786ESUP5OpsQ5UESE9wOg'; // Ваш ключ Google API
-        
-    //     return $api;
-        
-    // }
-    
-    // add_filter('acf/fields/google_map/api', 'my_acf_google_map_api');
-    
-    
 
