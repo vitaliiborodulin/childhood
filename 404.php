376a33dc59a7d87703327bdf75db91d5835df2bb
Vitalii Borodulin <vitaliiborodulin@yandex.ru>
2020-01-07 11:04:34 +0300

    add 404 page template

---
+++ 404.php
@@ -0,0 +1,14 @@
+<?php get_header(); ?>
+
+<div class="toys" style="min-height: 50vh; display: flex; justify-content: center; align-items: center">
+    <div class="container">
+        <h2 class="subtitle">
+            <a href="<?= get_home_url(); ?>" style="text-decoration: none">
+                Такой страницы не существует. Вернуться на главную?
+            </a>
+        </h2>
+        
+    </div>
+</div>
+
+<?php get_footer(); ?>
