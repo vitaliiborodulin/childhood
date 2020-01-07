0de9595b8cdd26a29b45616091f0992da935187e
Vitalii Borodulin <vitaliiborodulin@yandex.ru>
2020-01-07 11:11:05 +0300

    team img to custom field

--- page-team.php
+++ page-team.php
@@ -6,18 +6,29 @@ Template Name: Наша команда
 
 ?>
 
-<?php get_header(); ?>
+<?php 
+    get_header();
+?>
 
 <div class="specialists">
     <div class="container">
         <div class="title">Наша команда</div>
         <div class="row">
             <div class="col-lg-10 offset-lg-1">
-                <img class="specialists__img" src="<?php bloginfo('template_url'); ?>/assets/img/team.jpg"
-                    alt="наша команда">
+                <?php
+                        $image = get_field('team_img');
+                        if (!empty($image)): ?>
+                            <img 
+                            src="<?php echo $image['url']; ?>" 
+                            alt="<?php echo $image['alt']; ?>"
+                            class="specialists__img">
+                        <?php endif;
+                    ?>
             </div>
         </div>
     </div>
 </div>
 
-<?php get_footer();?>
+<?php 
+    get_footer();
+?>
